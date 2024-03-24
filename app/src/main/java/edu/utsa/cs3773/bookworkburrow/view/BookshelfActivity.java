c6485681e19cb6664fc1979b8454a4c24222d827
Israe <Israe@LAPTOP-VS4RO1RM>
2024-03-24 16:06:02 -0500

    - created layout and activity for Bookshelf

---
+++ app/src/main/java/edu/utsa/cs3773/bookworkburrow/view/BookshelfActivity.java
@@ -0,0 +1,18 @@
+package edu.utsa.cs3773.bookworkburrow.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.utsa.cs3773.bookworkburrow.R;
+
+public class BookshelfActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bookshelf);
+    }
+
+}
