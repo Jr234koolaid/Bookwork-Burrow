1b941b914671bd2a5db193ecf2daedc30a76c72b
RyanJ540 <ryan.johnson2@my.utsa.edu>
2024-04-08 09:54:20 -0500

    set up of book class

--- app/src/main/java/edu/utsa/cs3773/bookworkburrow/view/BookActivity.java
+++ app/src/main/java/edu/utsa/cs3773/bookworkburrow/view/BookActivity.java
@@ -6,14 +6,20 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import edu.utsa.cs3773.bookworkburrow.R;
+import edu.utsa.cs3773.bookworkburrow.model.Book;
 
 public class BookActivity extends AppCompatActivity
 {
+    Book readingBook;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
+
+        // expecting book object sent through extras
+        Bundle extras = getIntent().getExtras();
+        // TODO: readingBook = extras.getParcelable(readingBook,Book);
     }
 
 } // class BookActivity
