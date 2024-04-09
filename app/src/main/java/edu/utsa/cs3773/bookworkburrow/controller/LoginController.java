00a77551b0c77b6e22e5d2ddf878da8c14a3b2e1
Juan Rivera <jr234koolaid@gmail.com>
2024-04-08 19:09:11 -0500

    # Added Home activity # Moved firebase logic from activities to controllers

--- app/src/main/java/edu/utsa/cs3773/bookworkburrow/controller/LoginController.java
+++ app/src/main/java/edu/utsa/cs3773/bookworkburrow/controller/LoginController.java
@@ -9,12 +9,16 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 
+import edu.utsa.cs3773.bookworkburrow.FirebaseUtil;
 import edu.utsa.cs3773.bookworkburrow.R;
+import edu.utsa.cs3773.bookworkburrow.model.Account;
 import edu.utsa.cs3773.bookworkburrow.model.AccountDatabase;
 import edu.utsa.cs3773.bookworkburrow.model.ErrorDialog;
 import edu.utsa.cs3773.bookworkburrow.model.Input;
 import edu.utsa.cs3773.bookworkburrow.view.ForgotPasswordActivity;
+import edu.utsa.cs3773.bookworkburrow.view.HomeActivity;
 import edu.utsa.cs3773.bookworkburrow.view.MainActivity;
 import edu.utsa.cs3773.bookworkburrow.view.SignupActivity;
 
@@ -51,27 +55,19 @@ public class LoginController implements View.OnClickListener {
             EditText emailEditText = m_activity.findViewById(R.id.login_edit_email);
             EditText passwordEditText = m_activity.findViewById(R.id.login_edit_password);
 
-            try {
+            String email = emailEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
 
-                String email = Input.checkEmail(emailEditText);
-                String password = Input.checkPassword(passwordEditText);
+            CompletableFuture<Account> futureAccount = FirebaseUtil.loginWithUsernamePassword(email, password, m_activity);
+            futureAccount.thenAccept(account -> {
+                m_homeLauncher.launch(new Intent(m_activity, HomeActivity.class));
 
-                AccountDatabase accountDatabase = AccountDatabase.getInstance();
-                accountDatabase.setContext(m_activity);
-
-                String UID = accountDatabase.authenticate(email, password);
-
-                Intent intent = new Intent(m_activity, MainActivity.class);
-                intent.putExtra(MainActivity.INTENT_ACCOUNT_UID, UID);
-
-                m_homeLauncher.launch(intent);
-
-            } catch (IOException e) {
-
+            }).exceptionally(throwable -> {
                 ErrorDialog errorDialog = ErrorDialog.getInstance();
-                errorDialog.setContext(m_activity);
-                errorDialog.display(e.getMessage());
-            }
+                errorDialog.display("Could not log in");
+
+                return null;
+            });
 
         } else if (viewID == R.id.login_button_forgot_password) {
             m_forgotPasswordLauncher.launch(new Intent(m_activity, ForgotPasswordActivity.class));
@@ -82,3 +78,25 @@ public class LoginController implements View.OnClickListener {
     }
 
 } // class LoginController
+
+//            try {
+//
+//                String email = Input.checkEmail(emailEditText);
+//                String password = Input.checkPassword(passwordEditText);
+//
+//                AccountDatabase accountDatabase = AccountDatabase.getInstance();
+//                accountDatabase.setContext(m_activity);
+//
+//                String UID = accountDatabase.authenticate(email, password);
+//
+//                Intent intent = new Intent(m_activity, MainActivity.class);
+//                intent.putExtra(MainActivity.INTENT_ACCOUNT_UID, UID);
+//
+//                m_homeLauncher.launch(intent);
+//
+//            } catch (IOException e) {
+//
+//                ErrorDialog errorDialog = ErrorDialog.getInstance();
+//                errorDialog.setContext(m_activity);
+//                errorDialog.display(e.getMessage());
+//            }
--- app/src/main/java/edu/utsa/cs3773/bookworkburrow/controller/SignupController.java
+++ app/src/main/java/edu/utsa/cs3773/bookworkburrow/controller/SignupController.java
@@ -9,13 +9,16 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 
+import edu.utsa.cs3773.bookworkburrow.FirebaseUtil;
 import edu.utsa.cs3773.bookworkburrow.R;
 import edu.utsa.cs3773.bookworkburrow.model.Account;
 import edu.utsa.cs3773.bookworkburrow.model.AccountDatabase;
 import edu.utsa.cs3773.bookworkburrow.model.AccountStream;
 import edu.utsa.cs3773.bookworkburrow.model.ErrorDialog;
 import edu.utsa.cs3773.bookworkburrow.model.Input;
+import edu.utsa.cs3773.bookworkburrow.view.HomeActivity;
 import edu.utsa.cs3773.bookworkburrow.view.MainActivity;
 
 public class SignupController implements View.OnClickListener {
@@ -43,38 +46,54 @@ public class SignupController implements View.OnClickListener {
             EditText lastnameEditText = m_activity.findViewById(R.id.signup_edit_lastname);
             EditText passwordEditText = m_activity.findViewById(R.id.signup_edit_password);
 
-            try {
+            String email = emailEditText.getText().toString();
+            String firstName = firstnameEditText.getText().toString();
+            String lastName = lastnameEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
 
-                String email = Input.checkEmail(emailEditText);
-                String firstName = Input.checkName(firstnameEditText);
-                String lastName = Input.checkName(lastnameEditText);
-                String password = Input.checkPassword(passwordEditText);
+            CompletableFuture<Account> futureAccount = FirebaseUtil.createUser(email, password, m_activity);
+            futureAccount.thenAccept(account -> {
+                m_homeLauncher.launch(new Intent(m_activity, HomeActivity.class));
 
-                AccountDatabase accountDatabase = AccountDatabase.getInstance();
-                accountDatabase.setContext(m_activity);
-
-                String UID = accountDatabase.addAccount(email, password);
-
-                Account account = new Account(UID);
-                account.setEmail(email);
-                account.setFirstName(firstName);
-                account.setLastName(lastName);
-
-                AccountStream stream = new AccountStream(account, m_activity);
-                stream.write();
-
-                Intent intent = new Intent(m_activity, MainActivity.class);
-                intent.putExtra(MainActivity.INTENT_ACCOUNT_UID, UID);
-
-                m_homeLauncher.launch(intent);
-
-            } catch (IOException e) {
-
+            }).exceptionally(throwable -> {
                 ErrorDialog errorDialog = ErrorDialog.getInstance();
-                errorDialog.setContext(m_activity);
-                errorDialog.display(e.getMessage());
-            }
+                errorDialog.display("Could not sign up");
+
+                return null;
+            });
         }
     }
 
 } // class SignupController
+
+//            try {
+//
+//                String email = Input.checkEmail(emailEditText);
+//                String firstName = Input.checkName(firstnameEditText);
+//                String lastName = Input.checkName(lastnameEditText);
+//                String password = Input.checkPassword(passwordEditText);
+//
+//                AccountDatabase accountDatabase = AccountDatabase.getInstance();
+//                accountDatabase.setContext(m_activity);
+//
+//                String UID = accountDatabase.addAccount(email, password);
+//
+//                Account account = new Account(UID);
+//                account.setEmail(email);
+//                account.setFirstName(firstName);
+//                account.setLastName(lastName);
+//
+//                AccountStream stream = new AccountStream(account, m_activity);
+//                stream.write();
+//
+//                Intent intent = new Intent(m_activity, MainActivity.class);
+//                intent.putExtra(MainActivity.INTENT_ACCOUNT_UID, UID);
+//
+//                m_homeLauncher.launch(intent);
+//
+//            } catch (IOException e) {
+//
+//                ErrorDialog errorDialog = ErrorDialog.getInstance();
+//                errorDialog.setContext(m_activity);
+//                errorDialog.display(e.getMessage());
+//            }
