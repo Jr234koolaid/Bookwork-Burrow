a03e9b46fb3a5f849c92d2f19ea106353639cac5
Israe <Israe@LAPTOP-VS4RO1RM>
2024-04-16 20:57:54 -0500

    -fiexd some of the issues with the view -ignore the 'no speakable text warning, since that is for accessibility services'

---
+++ app/src/main/java/edu/utsa/cs3773/bookworkburrow/view/ExploreBooksActivity.java
@@ -0,0 +1,20 @@
+package edu.utsa.cs3773.bookworkburrow.view;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import edu.utsa.cs3773.bookworkburrow.R;
+
+public class ExploreBooksActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore_books);
+
+        // set a controller
+
+        // set a button for all of the books loaded dynamically
+        // set a listener for all of those buttons (books)
+
+    }
+}
